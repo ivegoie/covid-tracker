ea7741d6c43c8ce696d2950a91fe69fda4147838
Ivan Živolić <70758717+ivegoie@users.noreply.github.com>
2020-11-27 13:13:38 +0100

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -28,13 +28,6 @@ const Chart = ({data: {confirmed, deaths, recovered} , country}) => {
                     fill: true,
                 },
                 {
-                    data: dailyData.map(({ recovered }) => recovered),
-                    label: 'Recovered',
-                    borderColor: 'green',
-                    backgroundColor: 'rgba(0, 255, 0, .5)',
-                    fill: true,
-                }, 
-                {
                     data: dailyData.map(({ deaths }) => deaths),
                     label: 'Deaths',
                     borderColor: 'red',
